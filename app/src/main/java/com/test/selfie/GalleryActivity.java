5ac2b4f7cdf6b54c9dc9afa0f4a922e6bafed61e
lennonpetrick <lennonpetrick@gmail.com>
2018-08-01 01:31:39 -0300

    Created gallery activity layout

--- app/src/main/java/com/test/selfie/GalleryActivity.java
+++ app/src/main/java/com/test/selfie/GalleryActivity.java
@@ -1,7 +1,7 @@
 package com.test.selfie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class GalleryActivity extends AppCompatActivity {
 
